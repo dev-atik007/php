8bdff0490ac9cfd489673190830bbc706bf5ee33
dev-atik007 <atikur888setu@gmail.com>
2023-10-15 18:28:02 +0600

    veriable suabing

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
-echo "Hello Atik";
+$amount = 100;
+
+printf("Hello %s", "Atik","\n");
+// binary convart
+printf("Total amount is %b", "$amount");
+
+
+// Veriable suabing
+$test1 = 150;
+$test2 = 250;
+
+printf('Amount: %2$d Another Amount is: %1$d', $test1,$test2);
 ?>
